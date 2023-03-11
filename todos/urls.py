d5059e0b0c1cc3e282796e302f1250769233b15a
unknown <akinwun@gmail.com>
2023-03-11 13:42:40 +0100

    todos completed

--- todos/urls.py
+++ todos/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path('list/', views.list_todos_items)
+    path('list/', views.list_todos_items),
+    path('insert_todos/', views.insert_todos_items, name="insert_todos_items"),
+    path('delete_todos/<todo_id>/', views.delete_todos_items, name="delete_todos_items")
 ]
--- todos/views.py
+++ todos/views.py
@@ -1,17 +1,24 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
 # Create your views here.
 
-from django.http import HttpResponse
+from django.http import HttpRequest, HttpResponse
 import datetime
-
+from  .models import Todo
 def list_todos_items(request):
-    x =1 
-    y =2
-    now = datetime.datetime.now()
-    html = "<html><body>It is now %s </body></html>" % now
-    return render(request,'todos/todos.html',{'name':'Mosh'})
-    # return HttpResponse(html)
+    content = {'todo_list':Todo.objects.all()}
+    return render(request, 'todos/todos.html',content)
+
+def insert_todos_items(request:HttpRequest):
+    todo = Todo(content = request.POST['content'])
+    todo.save()
+    return redirect('/todos/list/')
+
+def delete_todos_items(request,todo_id):
+    todo_item_delete =  Todo.objects.get(id=todo_id)
+    todo_item_delete.delete()
+    return redirect('/todos/list/')
+ 
